ac03bb1c2de5f95af9fca104bb08eea304b77650
Ethics03 <honey032004@gmail.com>
2025-08-21 19:05:36 +0530

    feat: image gen added

--- src/genai/genai.controller.ts
+++ src/genai/genai.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { GenaiService } from './genai.service';
+import { SupabaseGuard } from 'src/auth/guards/auth.guard';
 
 @Controller('genai')
 export class GenaiController {
   constructor(private readonly genAiService: GenaiService) {}
+
+  @Post('generate-image')
+  async generateImage(@Body() body: { prompt: string }) {
+    return this.genAiService.GenerateImage(body.prompt);
+  }
 }
