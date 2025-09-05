d56eba130b3bf1e7aabbf2c57034acb3a21dde3d
Ethics03 <honey032004@gmail.com>
2025-09-05 11:03:15 +0530

    feat: added sleep report mail

--- src/genai/genai.module.ts
+++ src/genai/genai.module.ts
@@ -3,9 +3,10 @@ import { GenaiController } from './genai.controller';
 import { GenaiService } from './genai.service';
 import { PrismaService } from 'src/auth/prisma.service';
 import { ConfigService } from '@nestjs/config';
+import { McpService } from 'src/mcp/mcp.service';
 
 @Module({
   controllers: [GenaiController],
-  providers: [GenaiService, PrismaService, ConfigService],
+  providers: [GenaiService, PrismaService, ConfigService, McpService],
 })
 export class GenaiModule {}
